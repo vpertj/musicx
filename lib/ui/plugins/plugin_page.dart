3156f550df5d0ceae76e442ca0f6ff10a173d7a6
tj <89226782@qq.com>
2026-08-17 15:37:33 +0800

    chore: bump version to 1.5.1

--- lib/ui/plugins/plugin_page.dart
+++ lib/ui/plugins/plugin_page.dart
@@ -635,7 +635,7 @@ class _AboutCard extends StatelessWidget {
           ),
           const SizedBox(height: 10),
           Text(
-            'MusicX v1.5.0 · 插件协议兼容 MusicFree',
+            'MusicX v1.5.1 · 插件协议兼容 MusicFree',
             style: textTheme.labelSmall?.copyWith(color: scheme.outline),
           ),
         ],
